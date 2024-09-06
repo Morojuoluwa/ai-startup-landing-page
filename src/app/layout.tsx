ea4e7271634b75a73a187eaf133cf64881bf9bf9
abimbola patrick <abimbolapatrickmj@gmail.com>
2024-09-06 12:51:27 -0700

    added a meta image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "AI startup Landing Page",
   description: "A landing page for an AI startup",
+  icons:{
+    icon:'/pato.png'
+  }
 };
 
 export default function RootLayout({
